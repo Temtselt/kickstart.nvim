7632e8e820cfb1d61c54cc8575b2d4ad52277ad2
Temtselt <setemchlt@gmail.com>
2026-01-11 23:16:25 +0800

    Change colorscheme to unokai

--- init.lua
+++ init.lua
@@ -938,7 +938,7 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'PaperColor'
+      vim.cmd.colorscheme 'unokai'
     end,
   },
 
