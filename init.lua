f357b11a115c07e4fb8e7bb352e6954c8b87b470
temtselt <setemchlt@gmail.com>
2026-03-19 14:33:01 +0800

    Add github theme

--- init.lua
+++ init.lua
@@ -924,7 +924,7 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      -- vim.cmd.colorscheme 'unokai'
+      -- vim.cmd.colorscheme 'tokyonight'
     end,
   },
 
@@ -939,7 +939,21 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'unokai'
+      -- vim.cmd.colorscheme 'PaperColor'
+    end,
+  },
+
+  {
+    'projekt0n/github-nvim-theme',
+    name = 'github-theme',
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require('github-theme').setup {
+        -- ...
+      }
+
+      vim.cmd 'colorscheme github_dark_default'
     end,
   },
 
